67ae4cd2ec309fa56b94cf50664db74ff1ebca9f
DarrenZhou <1773963119@qq.com>
2016-04-22 17:17:36 +0800

    Create react-Tutorial
    Create react-Tutorial according to the offical Tutorial

---
+++ react-Tutorial/components/app.js
@@ -0,0 +1,49 @@
+var CommentBox=React.createClass({
+	render:function(){
+		return(
+			<div className='commentBox'>
+				<h1>Hello,world! I'am a CommentBox.</h1>
+				<CommentList />
+				<CommentForm />
+			</div>
+			);
+	}
+});
+
+var CommentList=React.createClass({
+	render:function(){
+		return(
+			<div className='commentList'>
+				<h2>Hello,world! I'am a CommentList.</h2>
+				<Comment author='Pete Hunt'>This is one comment</Comment>
+				<Comment author='Jordan Walke'>This is *another* comment</Comment>
+			</div>
+			)
+	}
+});
+
+var CommentForm=React.createClass({
+	render:function(){
+		return (
+			<div className='commentForm'>
+				Hello,world! I'am a CommentForm.
+			</div>
+			)
+	}
+});
+
+var Comment=React.createClass({
+	render:function(){
+		return(
+			<div className='comment'>
+				<h2 className='commentAuthor'>
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>
+			)
+	}
+})
+
+ReactDOM.render(<CommentBox />,document.getElementById('app'));
+
